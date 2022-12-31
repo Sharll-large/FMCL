9369d36cd5e3605179f3d36cfa48ec73d7c8d2e5
Sharll <106537475+Sharll-large@users.noreply.github.com>
2022-12-31 20:20:11 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ pip install zstandard
 pip install requests
 pip install nuitka
 pip install ordered-set
-nuitka --standalone --onefile --include-data-dir=View/Resources=View/Resources --plugin-enable=tk-inter --remove-output --disable-console main.pyw
+nuitka --standalone --onefile --remove-output main.py
