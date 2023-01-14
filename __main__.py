8c1acf226f8417f8bdb3ee8bbecbcf9c9d40546e
Sharll <106537475+Sharll-large@users.noreply.github.com>
2023-01-14 09:27:39 +0800

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,13 +1,16 @@
 # FMCL Boot Entry.
 import sys
+import tkinter.messagebox
 import FMCLCore.System.CoreConfigIO
 import FMCLView.main
-import tkinter.messagebox
+import FMCLView.Const
+
 if __name__ == "__main__":
     try:
         sys.stdout = open("FMCL.log", "w+")
         sys.stderr = open("FMCL.log", "w+")
         FMCLCore.System.CoreConfigIO.fixdepend()
+        FMCLView.Const.lang = FMCLCore.System.CoreConfigIO.read()["Language"]
         FMCLView.main.main()
     except Exception as e:
         tkinter.messagebox.showerror("FMCL Exception", repr(e))
