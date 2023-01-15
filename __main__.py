20d012e9699080934d2037fa9d7442688cf07ab5
Sharll <106537475+Sharll-large@users.noreply.github.com>
2023-01-15 15:28:00 +0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -7,8 +7,6 @@ import FMCLView.Const
 
 if __name__ == "__main__":
     try:
-        sys.stdout = open("FMCL.log", "w+")
-        sys.stderr = open("FMCL.log", "w+")
         FMCLCore.System.CoreConfigIO.fixdepend()
         FMCLView.Const.lang = FMCLCore.System.CoreConfigIO.read()["Language"]
         FMCLView.main.main()
