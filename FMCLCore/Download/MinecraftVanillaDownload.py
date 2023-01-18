77ed638c7aac4ba69385ce979f23970297a9ccb3
Sharll <106537475+Sharll-large@users.noreply.github.com>
2023-01-18 15:27:44 +0800

    Add files via upload

--- FMCLCore/Download/MinecraftVanillaDownload.py
+++ FMCLCore/Download/MinecraftVanillaDownload.py
@@ -1,28 +1,41 @@
 import json
 import urllib.request
 
-def get(vertype, download_source: str = "Default", refresh=False):
-    if refresh:
-        if download_source == "Default": manifest= "https://launchermeta.mojang.com/mc/game/version_manifest_v2.json"
-        elif download_source == "BMCLAPI": manifest= "https://bmclapi2.bangbang93.com/mc/game/version_manifest_v2.json"
-        elif download_source == "MCBBS": manifest= "https://download.mcbbs.net/mc/game/version_manifest_v2.json"
-        else: return False
-        information = urllib.request.urlopen(manifest).read()
-        meta = json.loads(information)
-        open(".fmcl.version_meta.json", "wb+").write(information)
-    else:
-        meta = json.load(open(".fmcl.version_meta.json", "rb+"))
 
-    verlist = []
+class Downloader:
+    def __init__(self, download_source: str = "Default"):
+        self.version_list = None
+        if download_source == "Default": self.manifest = "https://launchermeta.mojang.com/mc/game/version_manifest_v2.json"
+        elif download_source == "BMCLAPI": self.manifest = "https://bmclapi2.bangbang93.com/mc/game/version_manifest_v2.json"
+        elif download_source == "MCBBS": self.manifest = "https://download.mcbbs.net/mc/game/version_manifest_v2.json"
+        self.refresh()
 
-    lt_release, lt_snapshot = meta["latest"]["release"], meta["latest"]["snapshot"]
+    def refresh(self):
+        self.version_list = json.loads(urllib.request.urlopen(self.manifest).read())
 
-    for i in meta["versions"]:
-        if i["id"] == lt_release:
-            lt_release = i
-        if i["id"] == lt_snapshot:
-            lt_snapshot = i
-        if i["type"] in vertype:
-            verlist.append(i)
+    def get(self, version_types: list[str]):
+        temp = {}
+        for i in self.version_list["versions"]:
+            if i["type"] in version_types:
+                temp[i["id"]] = {
+                    "type": i["type"],
+                    "url": i["url"],
+                    "time": i["time"],
+                    "releaseTime": i["releaseTime"],
+                    "sha1": i["sha1"]
+                }
+        return temp
 
-    return lt_release, lt_snapshot, verlist
+    def get_latest(self):
+        return {
+            "snapshot": self.get(["release", "snapshot"])[self.version_list["latest"]["release"]],
+            "release": self.get(["release"])[self.version_list["latest"]["release"]]
+        }
+
+
+downloader = Downloader("Default")
+refresh = downloader.refresh
+get = downloader.get
+get_latest = downloader.get_latest
+
+
