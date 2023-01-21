53d881e5ebc4b3692fc0b9083701ca5f836ef84a
Sharll <106537475+Sharll-large@users.noreply.github.com>
2023-01-21 15:56:07 +0800

    Delete FMCLView directory

--- FMCLView/Pages/AboutPage.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-import tkinter.ttk
-import FMCLView.Const
-
-def main():
-    f = tk.ttk.Frame()
-    tk.ttk.Label(f).pack()
-    return f
--- FMCLView/Pages/UnderDevelop.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-import tkinter.ttk
-def main():
-    f = tk.Frame()
-    tk.ttk.Label(f, text="Under develop...").pack()
-    return f
