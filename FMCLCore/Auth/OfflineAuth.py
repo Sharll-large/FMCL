d862f386b235427924f58d18d18a32539d06aa2b
Sharll <106537475+Sharll-large@users.noreply.github.com>
2023-02-10 13:48:54 +0800

    Add files via upload

---
+++ FMCLCore/Auth/OfflineAuth.py
@@ -0,0 +1,9 @@
+import uuid
+import base64
+
+def Auth(playername: str):
+    return {
+        "username": playername,
+        "uuid": uuid.uuid4().hex,
+        "access_token": base64.b64encode(("{\"name\": \"" + playername + "\"}").encode())
+    }
