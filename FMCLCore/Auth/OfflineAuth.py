310233771dd2cc0e345d392b00e73d0a8171a467
Sharll <106537475+Sharll-large@users.noreply.github.com>
2023-02-10 13:50:02 +0800

    Update OfflineAuth.py

--- FMCLCore/Auth/OfflineAuth.py
+++ FMCLCore/Auth/OfflineAuth.py
@@ -5,5 +5,5 @@ def Auth(playername: str):
     return {
         "username": playername,
         "uuid": uuid.uuid4().hex,
-        "access_token": base64.b64encode(("{\"name\": \"" + playername + "\"}").encode())
+        "access_token": base64.b64encode(("{\"name\": \"" + playername + "\"}").encode()).decode()
     }
