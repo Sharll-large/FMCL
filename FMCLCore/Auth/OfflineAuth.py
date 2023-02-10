0f0d5e56915e31c4a55da6b9ec965481c828f485
Sharll <106537475+Sharll-large@users.noreply.github.com>
2023-02-10 15:55:44 +0800

    Delete FMCLCore/Auth directory

--- FMCLCore/Auth/OfflineAuth.py
+++
@@ -1,9 +0,0 @@
-import uuid
-import base64
-
-def Auth(playername: str):
-    return {
-        "username": playername,
-        "uuid": uuid.uuid4().hex,
-        "access_token": base64.b64encode(("{\"name\": \"" + playername + "\"}").encode()).decode()
-    }
