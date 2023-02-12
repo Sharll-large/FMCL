5c6ccdd7cb4abc82a05f153e6aaae39dcaf5380d
Sharll <106537475+Sharll-large@users.noreply.github.com>
2023-02-12 19:22:02 +0800

    更新package.py

--- package.py
+++ package.py
@@ -1,5 +1,5 @@
 import zipapp
-import zipfile
+import i18n
 import os
 
 def search_all_programs(__path = None):
@@ -13,22 +13,26 @@ def search_all_programs(__path = None):
                 programs += search_all_programs(os.path.join(__path, i))
     else:
         for i in os.listdir():
-            if i not in ["package.py", ".FMCL_Package_Time"]:
+            if i not in ["package.py", ".FMCL_Package_Time", "i18n.py"]:
                 programs += search_all_programs(i)
     return programs
 
-print("FMCL 打包脚本 1.0")
+if __name__ == '__main__':
+    print("FMCL 打包脚本 1.0")
+    
+    print("1 转换i18n.py")
+    i18n.main("i18n.xlsx", "FMCLView/i18n.py")
 
-print("1 过滤冗余的非python文件")
-p = search_all_programs()
+    print("2 过滤冗余的非python文件")
+    p = search_all_programs()
 
-print("2 复制需要的文件")
-for i in p:
-    os.makedirs(os.path.dirname(os.path.join(".FMCL_Package_Time", i)), exist_ok=True)
-    open(os.path.join(".FMCL_Package_Time", i), "wb").write(open(i, "rb").read())
+    print("3 复制需要的文件")
+    for i in p:
+        os.makedirs(os.path.dirname(os.path.join(".FMCL_Package_Time", i)), exist_ok=True)
+        open(os.path.join(".FMCL_Package_Time", i), "wb").write(open(i, "rb").read())
 
-print("3 生成Zip-app")
-zipapp.create_archive(".FMCL_Package_Time", "FMCL.pyzw", "/usr/bin/env/ python3", compressed=True)
+    print("3 生成Zip-app")
+    zipapp.create_archive(".FMCL_Package_Time", "FMCL.pyzw", "/usr/bin/python3", compressed=True)
 
 #
 # zipapp.create_archive(".", "FMCL.pyzw", "/usr/bin/env python3")
